1c2bf7c26d0ceb94c1c6e8f6ca92c6cbcd9a82ef
KOKUU2003 <2300452184@qq.com>
2025-09-16 16:47:24 +0800

    【修改】 Json 报错时调用的数学库

--- Assets/Scripts/Kokuu/Json/JsonGrammarException.cs
+++ Assets/Scripts/Kokuu/Json/JsonGrammarException.cs
@@ -12,7 +12,7 @@ namespace Kokuu.Json
         private static string GenerateMessage(string expectation, string json, int index, List<string> path)
         {
             string context = String.Empty;
-            if (index > 0) context += json[Math.Max(index - 10, 0)..index];
+            if (index > 0) context += json[Mathf.Max(index - 10, 0)..index];
             if (index < json.Length) context += $"<color=red>{json[index]}</color>";
             if (index < json.Length - 1) context += json[(index + 1)..Mathf.Min(index + 11, json.Length)];
 
